83d8581b9088607b1cc88e9c29b3b75cd9185677
Nasim <70124945+Nasimul650@users.noreply.github.com>
2025-12-06 10:04:16 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import NavBar from "./components/NavBar";
+import ProductViewer from "./components/ProductViewer";
+import Showcase from "./components/Showcase";
+import Performance from "./components/Performance";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <ProductViewer />
+      <Showcase />
+      <Performance />
+      <Features />
+      <Highlights />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
